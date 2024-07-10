b4073c386ed2044523ef6a146b393f09da97813a
AxlEstevez <axlestevez@gmail.com>
2024-07-10 17:39:57 -0600

    Initial Commit

---
+++ swiftUIApp/swiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIAppApp.swift
+//  swiftUIApp
+//
+//  Created by aestevezn on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
